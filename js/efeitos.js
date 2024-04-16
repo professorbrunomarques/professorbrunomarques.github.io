43afe4c246502bf6672a2d05f9cc3cb6cc61ec7f
professorbrunomarques <42039050+professorbrunomarques@users.noreply.github.com>
2024-04-16 07:17:50 -0300

    Add files via upload

---
+++ js/efeitos.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    let menu = document.getElementById("menu");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
