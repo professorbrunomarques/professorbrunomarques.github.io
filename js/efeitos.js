354315a73eafdc638fd83bed400e0b68ddeb2cd3
professorbrunomarques <42039050+professorbrunomarques@users.noreply.github.com>
2025-09-24 09:03:06 -0300

    Remove old jQuery exercises and update assets
    Deleted legacy jQuery exercise files, images, and scripts from 'aula 07 jq' and 'aula08jq' directories. Removed unused images and CSS/JS files. Added new CSS and image assets, and updated 'index.html' to reflect these changes. This cleanup prepares the project for a new structure and updated content.

--- js/efeitos.js
+++
@@ -1,8 +0,0 @@
-function clickMenu() {
-    let menu = document.getElementById("menu");
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-}
